655038907d8702ab4ea3bb1470746b8fecdb9a0c
Paul Calabrese <pacalabre@gmail.com>
2018-12-10 17:03:05 -0800

    added hash deep link,capitalized current selected tag, added overflow hidden to grid items, and cleaned up and commented code out

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let tagsArr = [];
-// let gridArr = [];
 let gridItems = $('.grid').children();
 let filterMenu = $('.filter-menu-button-container');
 let allCategories = $('#showAllButton');
@@ -13,33 +12,42 @@ function initalizeGrid() {
     });
 }
 
-$('#filterButton').on('click', function(){
+$('#filterButton').on('click', function() {
     $('.filter-menu').toggleClass('show');
     $('#filterButton').toggleClass('active');
-})
+}) 
 
 /* Close the menu if you click away from it */
+
 $(document).click(function(event) {
   if (!$(event.target).closest(".filter-menu,.filter-container").length) {
     $("body").find(".filter-menu").removeClass("show");
     $('#filterButton').removeClass('active');
   }
-});
- 
+}); 
+
+/* Collect tags */
+
 gridItems.each(function(index,gridItem) {
     let itemTag = gridItems[index].dataset.tags;
     if (!tagsArr.includes(itemTag)){
         tagsArr.push(itemTag);
     }
-})
+}) 
+
+/* Create Buttons from Tags */
 
 for( tag in tagsArr) {
+    let buttonId = tagsArr[tag].replace(/\s/g, '');
     let button = document.createElement('button');
     button.className = 'filter-menu-button';
-    button.innerHTML = tagsArr[tag];
+    button.innerHTML = tagsArr[tag].toLowerCase();
     filterMenu.append(button);
     button.onclick = dynamicTagButtons;
+    button.id = buttonId;
 }
+
+/* Click Events for Dynamic Buttons */
 
 function dynamicTagButtons() {
     let buttonClicked = $(this);
@@ -58,19 +66,20 @@ function dynamicTagButtons() {
 }
 
 function updateCategoryText(tag) {
-    $('.filter-button-right').empty();
-    $('.filter-button-right').text(tag.text());
+    let firstLetterRx = tag.text();
+    let uppercaseFirstLetter = firstLetterRx.replace(/\b[a-z]/g,function(f){return f.toUpperCase();});
+    $('.filter-container-right-text').empty();
+    $('.filter-container-right-text').text(uppercaseFirstLetter);
 }
 
 function resetGridClasses() {
-    /* Just need to remove active class from button clicked */
     $('.filter-menu-button').removeClass('active');
     gridItems.each(function(index,gridItem) {
         $(gridItem).removeClass('hide');
     })
 }
 
-$('#showAllButton').on('click', function(){
+$('#showAllButton').on('click', function() {
     updateCategoryText(allCategories);
     $('.filter-menu-button').removeClass('active');
     gridItems.each(function(index,gridItem) {
@@ -79,5 +88,20 @@ $('#showAllButton').on('click', function(){
     initalizeGrid();
 })
 
+/* Deep Linking */
+
+var getTheHashInUrl = function () {
+    var hash = window.location.hash;
+    let stringToArr = hash.split('');
+    let removeHashFromStr =  stringToArr.splice(1, stringToArr.length).join('').toLowerCase();
+    $('#'+ removeHashFromStr).click();
+};
+
+$(window).on('load', getTheHashInUrl);
+
 /* Initialize grid on load */
+
 initalizeGrid();
+
+ 
+
